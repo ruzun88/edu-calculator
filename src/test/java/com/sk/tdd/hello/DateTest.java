59bf612b3cbbedad62cbe1877ac02b56fb4bb96e
yongjun <ruzun88@gmail.com>
2022-03-16 11:32:39 +0900

    #6 테스트 failure fix

--- src/test/java/com/sk/tdd/hello/DateTest.java
+++ src/test/java/com/sk/tdd/hello/DateTest.java
@@ -13,7 +13,7 @@ public class DateTest {
 		Date date = new Date();
 		Class<? extends Date> class1 = date.getClass();
 		String name = class1.getName();
-		assertEquals("", name);
+		assertEquals("java.util.Date", name);
 	}
 
 }
